e2d019bb347a1bd2912b7e139247f908240ed316
emoteNS <emote@nearsoft.com>
2018-09-03 00:10:36 -0500

    First commit

---
+++ src/com/company/Flight.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Flight {
+    Date d, a;
+    int minutes;
+    ArrayList<FlightSeat> seats = new ArrayList<>();
+    String type;
+
+    public Flight(Date d, Date a, int minutes, String type) {
+        // Do stuff to initialize values
+    }
+
+    public void travel() {
+        //Do stuff to travel
+    }
+
+    //TODO Refactor this
+    public ArrayList<FlightSeat> getFreeSeats() {
+        ArrayList<FlightSeat> freeSeats = new ArrayList<>();
+        for(int i = 0; i < seats.size(); i++) {
+            FlightSeat tempSeat = this.seats.get(i);
+            if(tempSeat.getFree()) {
+                freeSeats.add(tempSeat);
+            }
+        }
+        return freeSeats;
+    }
+
+    public void printItinerary() {
+        // Do stuff to print flight data
+    }
+}
+
+
---
+++ src/com/company/FlightSeat.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FlightSeat {
+    private boolean free;
+
+    public boolean getFree() {
+        return free;
+    }
+}
